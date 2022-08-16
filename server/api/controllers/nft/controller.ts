fa33e245911a80d66fd6a88d2673340980470692
doquang0310 <38870227+doquang0310@users.noreply.github.com>
2022-08-16 21:51:56 +0700

    validate

--- server/api/controllers/nft/controller.ts
+++ server/api/controllers/nft/controller.ts
@@ -34,8 +34,6 @@ export class Controller {
           } else {
             fs.mkdirSync(`${layersDir}/${item.name}`);
           }
-          console.log('type of list image',typeof item.listImage)
-          console.log('data list image', item.listImage)
 
           if (typeof item.listImage == "object" && item.listImage.length > 0) {
             Promise.all(
--- server/queue/nftGen.ts
+++ server/queue/nftGen.ts
@@ -6,7 +6,7 @@ const basePath = process.cwd();
 const { startCreating } = require(`${basePath}/server/common/hashlip/src/main`);
 
 const options = {
-  removeOnSuccess: false,
+  removeOnSuccess: true,
 };
 
 const genNft = new Queue("genNft", options);
@@ -16,57 +16,81 @@ genNft.process(async (job, done) => {
   const collectionDir = `${basePath}/storage/${req.idCollection}`;
   const buildDir = `${basePath}/storage/${req.idCollection}/build`;
   const layersDir = `${basePath}/storage/${req.idCollection}/layers`;
-  let layerOrder = [];
 
-  setupFolder(collectionDir, buildDir, layersDir);
-  req.body.layoutOrder.map(async (item, index) => {
-    layerOrder.push({ name: item.name });
-    if (fs.existsSync(`${layersDir}/${item.name}`)) {
-      fs.rmdirSync(
-        `${layersDir}/${item.name}`,
-        { recursive: true },
-        (error) => {
-          //you can handle the error here
-        }
-      );
-      fs.mkdirSync(`${layersDir}/${item.name}`);
-    } else {
-      fs.mkdirSync(`${layersDir}/${item.name}`);
-    }
-    Promise.all(
-      item.listImage.map((element, index) => {
-        var base64Data = element.replace(/^data:image\/png;base64,/, "");
-        fs.writeFileSync(
-          `${layersDir}/${item.name}/${index}.png`,
-          base64Data,
-          "base64",
-          (err) => {
-            if (err) console.log("errSaveImage", err);
-            console.log("Saved!");
+  let layerOrder = [];
+  let flagError = [];
+
+  if (typeof req.body.layoutOrder == "object") {
+    setupFolder(collectionDir, buildDir, layersDir);
+    req.body.layoutOrder.map(async (item, index) => {
+      layerOrder.push({ name: item.name });
+      if (fs.existsSync(`${layersDir}/${item.name}`)) {
+        fs.rmdirSync(
+          `${layersDir}/${item.name}`,
+          { recursive: true },
+          (error) => {
+            //you can handle the error here
           }
         );
-      })
-    );
-  });
-  startCreating(
-    [
-      {
-        growEditionSizeTo: req.body.totalImage,
-        layersOrder: layerOrder,
-      },
-    ],
-    `${buildDir}/images`,
-    `${buildDir}/json`,
-    layersDir,
-    req.body,
-    buildDir
-  );
+        fs.mkdirSync(`${layersDir}/${item.name}`);
+      } else {
+        fs.mkdirSync(`${layersDir}/${item.name}`);
+      }
+      if (typeof item.listImage == "object" && item.listImage.length > 0) {
+        Promise.all(
+          item.listImage.map((element, index) => {
+            var base64Data = element.replace(/^data:image\/png;base64,/, "");
+            const base64regex =
+              /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+            if (base64regex.test(base64Data) == true) {
+              fs.writeFileSync(
+                `${layersDir}/${item.name}/${index}.png`,
+                base64Data,
+                "base64",
+                (err) => {
+                  if (err) console.log("errSaveImage", err);
+                  console.log("Saved!");
+                }
+              );
+            } else {
+              flagError.push(`Not base64 format : ${item.name} - ${index}`);
+            }
+          })
+        );
+      } else {
+        flagError.push(`List Image Null : ${item.name}`);
+      }
+    });
+    if (flagError.length == 0) {
+      startCreating(
+        [
+          {
+            growEditionSizeTo: req.body.totalImage,
+            layersOrder: layerOrder,
+          },
+        ],
+        `${buildDir}/images`,
+        `${buildDir}/json`,
+        layersDir,
+        req.body,
+        buildDir
+      );
 
-  await CollectionService.updateStatus(job.data.idCollection, 2);
+      await CollectionService.updateStatus(job.data.idCollection, 2);
+    } else {
+      await CollectionService.updateStatus(job.data.idCollection, 4);
+    }
+  }else {
+    await CollectionService.updateStatus(job.data.idCollection, 4);
+  }
 });
 
 genNft.on("succeeded", (job, result) => {
   CollectionService.updateStatus(job.data.idCollection, 3);
+});
+
+genNft.on('failed', (job, err) => {
+    console.log(`Job ${job.id} failed with error ${err.message}`);
 });
 
 export const setupFolder = (collectionDir, buildDir, layersDir) => {
